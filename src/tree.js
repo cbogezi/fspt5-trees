8a0b656edc529c2439db8ea49b4ffae9ddfb7d53
Carol Bogezi <cbogezi@gmail.com>
2021-03-03 21:13:40 -0500

    Common Trees activity try

--- src/tree.js
+++ src/tree.js
@@ -12,22 +12,63 @@ class Tree {
   }
 
   // adds a child into the tree
-  addChild(value) {
-    // push a new tree into the current tree's children
-  }
+    addChild(value) {
+        let newNode = new Tree(value);
+        // push a new tree into the current tree's children
+        this.children.push(newNode);
+      }
+    
+      forEach(fn) {
+        //do this to the node
+        fn(this);
+        //do this to the children
+        for (let child of this.children) {
+          child.forEach(fn);
+        }
+      } 
+    }
 
-  // returns true/false if the value is in the tree
-  contains(value) {
-    // if the tree value matches the value passed in
-    // return true
-    // else
+  let tree = new Tree(1);
+  tree.addChild(2);
+  tree.addChild(3);
+  tree.addChild(4);
+  tree.children[0].addChild(20);
+  tree.children[0].children[0].addChild(200);
+  //console.log(tree);
+
+  tree.forEach(tree => {
+    console.log(tree.value);
+  });
+
+ // returns true/false if the value is in the tree
+  contains(value) 
+
+    if (this.children === value) { //the tree value matches the value passed in
+      return true
+
+    } else {
     // loop through all the children
     // call the contains method located on each child (pass the value in)
-    // return false if not found at all
-  }
+      while (currNode.value !== value) {
+        currNode == newNode.next;
+        newNode.next = newNode;
+    
+   // return false if not found at all
+    } else {
+       return false;
+ 
+    }
+
 
   // applies the function to each child in the tree
-  traverse(fn) {}
+
+  traverse(fn) {
+    
+    fn(this);
+    for (let child of this.children) {
+      child.forEach(fn);
+    }
+  }
 
   // OPTIONAL
   //return the node with that value if found. Returns null otherwise
